0cb253f559ef8ad88403e196720fbeb730e86fed
agneta1991 <pupienytea@gmail.com>
2024-04-18 16:27:08 +0300

    set up & landing page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from "./components/home";
+import "./assets/homepage.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home.js
@@ -0,0 +1,101 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div class="container">
+      <div class="square one"></div>
+      <div class="square two"></div>
+      <div class="square three"></div>
+      <div class="square four">
+        <p>About Me</p>
+      </div>
+      <div class="square five">
+        <p>Projects</p>
+      </div>
+      <div class="square six">
+        <p>Contact</p>
+      </div>
+      <div class="square seven">
+        <img
+          class="icon"
+          src="https://img.icons8.com/emoji/48/lithuania-emoji.png"
+          alt="lithuania-emoji"
+        />
+      </div>
+      <div class="square eight">
+        <img
+          class="icon"
+          src="https://img.icons8.com/ios/50/1a7e2f/phone--v1.png"
+          alt="phone--v1"
+        />
+      </div>
+      <div class="square nine">
+        <img
+          class="icon"
+          src="https://img.icons8.com/ios/50/1a7e2f/new-post--v1.png"
+          alt="new-post--v1"
+        />
+      </div>
+      <div class="square two"></div>
+      <div class="square"></div>
+      <div class="square right-square-one"></div>
+      <div class="square right-square-two"></div>
+      <div class="square"></div>
+      <div class="square left-square-two"></div>
+      <div class="square right-square-three"></div>
+      <div class="square right-square-four"></div>
+      <div class="square central">
+        <div class="central-div">
+          <h1 class="header name">Agneta Pupienyte</h1>
+          <h2 class="header">Full-stack developer</h2>
+          <h3>{"{ JavaScript, React.js, Ruby, Ruby on Rails..}"}</h3>
+          <h3>
+            Specialising in Ruby on Rails, leverage cutting-edge technologies,
+            to bring to bring web projects to life
+          </h3>
+          <div class="socials">
+            <img
+              src="https://img.icons8.com/ios-filled/50/1a7e2f/github.png"
+              alt="github"
+            />
+            <img
+              src="https://img.icons8.com/ios-filled/50/1a7e2f/linkedin.png"
+              alt="linkedin"
+            />
+            <img
+              src="https://img.icons8.com/ios-filled/50/1a7e2f/medium-logo.png"
+              alt="medium-logo"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="square left-square-three"></div>
+      <div class="square left-square-four"></div>
+      <div class="square right-square-five"></div>
+      <div class="square right-square-six"></div>
+      <div class="square"></div>
+      <div class="square left-square-six"></div>
+      <div class="square right-square-seven"></div>
+      <div class="square right-square-eight"></div>
+
+      <div class="square bottom-row-one">
+        <img
+          width="50"
+          height="50"
+          src="https://img.icons8.com/ios/50/down--v1.png"
+          alt="down--v1"
+        />
+      </div>
+      <div class="square bottom-row-two"></div>
+      <div class="square bottom-row-three"></div>
+      <div class="square bottom-row-four"></div>
+      <div class="square bottom-row-five"></div>
+      <div class="square bottom-row-six"></div>
+      <div class="square bottom-row-seven"></div>
+      <div class="square bottom-row-six"></div>
+      <div class="square bottom-row-nine"></div>
+    </div>
+  );
+};
+
+export default Home;
