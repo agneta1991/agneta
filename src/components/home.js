d2ae1e89230ca1489208098914261ebcc09bca61
agneta1991 <pupienytea@gmail.com>
2024-04-19 09:41:38 +0300

    Animation added to landing page

--- src/components/home.js
+++ src/components/home.js
@@ -2,58 +2,58 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div class="container">
-      <div class="square one"></div>
-      <div class="square two"></div>
-      <div class="square three"></div>
-      <div class="square four">
+    <div className="container">
+      <div className="square one"></div>
+      <div className="square two"></div>
+      <div className="square three"></div>
+      <div className="square four">
         <p>About Me</p>
       </div>
-      <div class="square five">
+      <div className="square five">
         <p>Projects</p>
       </div>
-      <div class="square six">
+      <div className="square six">
         <p>Contact</p>
       </div>
-      <div class="square seven">
+      <div className="square seven">
         <img
-          class="icon"
+          className="icon"
           src="https://img.icons8.com/emoji/48/lithuania-emoji.png"
           alt="lithuania-emoji"
         />
       </div>
-      <div class="square eight">
+      <div className="square eight">
         <img
-          class="icon"
+          className="icon"
           src="https://img.icons8.com/ios/50/1a7e2f/phone--v1.png"
           alt="phone--v1"
         />
       </div>
-      <div class="square nine">
+      <div className="square nine">
         <img
-          class="icon"
+          className="icon"
           src="https://img.icons8.com/ios/50/1a7e2f/new-post--v1.png"
           alt="new-post--v1"
         />
       </div>
-      <div class="square two"></div>
-      <div class="square"></div>
-      <div class="square right-square-one"></div>
-      <div class="square right-square-two"></div>
-      <div class="square"></div>
-      <div class="square left-square-two"></div>
-      <div class="square right-square-three"></div>
-      <div class="square right-square-four"></div>
-      <div class="square central">
-        <div class="central-div">
-          <h1 class="header name">Agneta Pupienyte</h1>
-          <h2 class="header">Full-stack developer</h2>
+      <div className="square two"></div>
+      <div className="square"></div>
+      <div className="square right-square-one"></div>
+      <div className="square right-square-two"></div>
+      <div className="square"></div>
+      <div className="square left-square-two"></div>
+      <div className="square right-square-three"></div>
+      <div className="square right-square-four"></div>
+      <div className="square central">
+        <div className="central-div">
+          <h1 className="header name">Agneta Pupienyte</h1>
+          <h2 className="header">Full-stack developer</h2>
           <h3>{"{ JavaScript, React.js, Ruby, Ruby on Rails..}"}</h3>
           <h3>
             Specialising in Ruby on Rails, leverage cutting-edge technologies,
             to bring to bring web projects to life
           </h3>
-          <div class="socials">
+          <div className="socials">
             <img
               src="https://img.icons8.com/ios-filled/50/1a7e2f/github.png"
               alt="github"
@@ -69,16 +69,16 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div class="square left-square-three"></div>
-      <div class="square left-square-four"></div>
-      <div class="square right-square-five"></div>
-      <div class="square right-square-six"></div>
-      <div class="square"></div>
-      <div class="square left-square-six"></div>
-      <div class="square right-square-seven"></div>
-      <div class="square right-square-eight"></div>
+      <div className="square left-square-three"></div>
+      <div className="square left-square-four"></div>
+      <div className="square right-square-five"></div>
+      <div className="square right-square-six"></div>
+      <div className="square"></div>
+      <div className="square left-square-six"></div>
+      <div className="square right-square-seven"></div>
+      <div className="square right-square-eight"></div>
 
-      <div class="square bottom-row-one">
+      <div className="square bottom-row-one">
         <img
           width="50"
           height="50"
@@ -86,14 +86,14 @@ const Home = () => {
           alt="down--v1"
         />
       </div>
-      <div class="square bottom-row-two"></div>
-      <div class="square bottom-row-three"></div>
-      <div class="square bottom-row-four"></div>
-      <div class="square bottom-row-five"></div>
-      <div class="square bottom-row-six"></div>
-      <div class="square bottom-row-seven"></div>
-      <div class="square bottom-row-six"></div>
-      <div class="square bottom-row-nine"></div>
+      <div className="square bottom-row-two"></div>
+      <div className="square bottom-row-three"></div>
+      <div className="square bottom-row-four"></div>
+      <div className="square bottom-row-five"></div>
+      <div className="square bottom-row-six"></div>
+      <div className="square bottom-row-seven"></div>
+      <div className="square bottom-row-six"></div>
+      <div className="square bottom-row-nine"></div>
     </div>
   );
 };
