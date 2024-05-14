eb3908e50500fe8dea9fbafa40e7db30dc973713
agneta1991 <pupienytea@gmail.com>
2024-05-14 08:53:50 +0300

    Added links to socials

--- src/components/socials.js
+++ src/components/socials.js
@@ -4,18 +4,36 @@ const Socials = () => {
   return (
     <div className="socials-div">
       <div className="socials">
-        <img
-          src="https://img.icons8.com/ios-filled/50/000000/github.png"
-          alt="github"
-        />
-        <img
-          src="https://img.icons8.com/ios-filled/50/000000/linkedin.png"
-          alt="linkedin"
-        />
-        <img
-          src="https://img.icons8.com/ios-filled/50/000000/medium-logo.png"
-          alt="medium-logo"
-        />
+        <a
+          href="https://github.com/agneta1991"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://img.icons8.com/ios-filled/50/000000/github.png"
+            alt="github"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/agneta-pupienyte/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://img.icons8.com/ios-filled/50/000000/linkedin.png"
+            alt="linkedin"
+          />
+        </a>
+        <a
+          href="https://medium.com/@a.pupienyte"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://img.icons8.com/ios-filled/50/000000/medium-logo.png"
+            alt="medium-logo"
+          />
+        </a>
         <img
           width="50"
           height="50"
