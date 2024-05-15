3a326f2fc93e186dbe99aa47e75e2c354f5b90ad
agneta1991 <pupienytea@gmail.com>
2024-05-15 13:27:23 +0300

    Projects page added & responsiveness for 1024 fixed

--- src/components/dataFetching.js
+++ src/components/dataFetching.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import {
   updateInnerHTML,
+  updateInnerHTML0,
   updateInnerHTML1,
   updateInnerHTML2,
   updateInnerHTML3,
@@ -9,7 +10,7 @@ import {
 
 const DataFetchingComponent = () => {
   const githubname = "agneta1991";
-  const accessToken = "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";
+  const accessToken = "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";
   const targetRepositories = [
     "book-an-appointment-capstone",
     "Budget-app-capstone",
@@ -32,6 +33,7 @@ const DataFetchingComponent = () => {
           .filter((repo) => targetRepositories.includes(repo.name))
           .map((repo) => ({
             name: repo.name,
+            link: repo.html_url,
             description: repo.description || "",
           }));
         setProjects(filteredProjects);
@@ -44,6 +46,7 @@ const DataFetchingComponent = () => {
   useEffect(() => {
     if (projects.length > 0) {
       updateInnerHTML();
+      updateInnerHTML0();
       updateInnerHTML1();
       updateInnerHTML2();
       updateInnerHTML3();
@@ -56,8 +59,75 @@ const DataFetchingComponent = () => {
       <ul>
         {projects.map((project, index) => (
           <li key={index}>
-            <h2 id={project.name}>{project.name}</h2>
-            <p>{project.description}</p>
+            <img className={project.name} alt={project.name} />
+            <div className="projects-title-div">
+              <h2 id={project.name}>{project.name}</h2>
+              <a
+                className="see-source"
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                | See Source{" "}
+                <img
+                  width="50"
+                  height="50"
+                  src="https://img.icons8.com/ios/50/5656e7/code--v1.png"
+                  alt="code--v1"
+                />
+              </a>
+            </div>
+            <p className="description">{project.description}</p>
+            <div className="languages-list">
+              {index === 0 && (
+                <ul className="listings list">
+                  <li>Ruby on Rails</li>
+                  <li>PostgreSQL</li>
+                  <li>JWT</li>
+                  <li>React.js</li>
+                </ul>
+              )}
+              {index === 1 && (
+                <ul className="listings list">
+                  <li>Ruby on Rails</li>
+                  <li>PostgreSQL</li>
+                  <li>Rspec</li>
+                  <li>Figma</li>
+                </ul>
+              )}
+              {index === 2 && (
+                <ul className="listings list">
+                  <li>Ruby</li>
+                  <li>SQL</li>
+                  <li>JSON</li>
+                  <li>Command-Line Interface</li>
+                </ul>
+              )}
+              {index === 3 && (
+                <ul className="listings list">
+                  <li>JavaScript</li>
+                  <li>HTML5</li>
+                  <li>CSS</li>
+                  <li>Responsiveness</li>
+                </ul>
+              )}
+              {index === 4 && (
+                <ul className="listings list">
+                  <li>React.js</li>
+                  <li>Redux</li>
+                  <li>API</li>
+                  <li>Figma</li>
+                </ul>
+              )}
+              {index === 5 && (
+                <ul className="listings list">
+                  <li>React.js</li>
+                  <li>Redux</li>
+                  <li>API</li>
+                  <li>Jest</li>
+                </ul>
+              )}
+            </div>
           </li>
         ))}
       </ul>
--- src/components/titles.js
+++ src/components/titles.js
@@ -1,7 +1,32 @@
+import health from "../assets/images/health.jpg";
+import budget from "../assets/images/budget.png";
+import weather from "../assets/images/weather.jpg";
+import heavy from "../assets/images/heavy.jpg";
+import ruby from "../assets/images/ruby.png";
+import space from "../assets/images/space.jpg";
+
 export const updateInnerHTML = () => {
   const element = document.getElementById("book-an-appointment-capstone");
   if (element) {
     element.innerHTML = "Health Clinic";
+
+    const img = document.querySelector(`img.${element.id}`);
+    if (img) {
+      img.src = health;
+      img.alt = "Health Clinic";
+    }
+  }
+};
+
+export const updateInnerHTML0 = () => {
+  const element = document.getElementById("HeavyFury");
+  if (element) {
+    element.innerHTML = "HeavyFury";
+    const img = document.querySelector(`img.${element.id}`);
+    if (img) {
+      img.src = heavy;
+      img.alt = "HeavyFury";
+    }
   }
 };
 
@@ -9,6 +34,11 @@ export const updateInnerHTML1 = () => {
   const element = document.getElementById("Budget-app-capstone");
   if (element) {
     element.innerHTML = "Finance App";
+    const img = document.querySelector(`img.${element.id}`);
+    if (img) {
+      img.src = budget;
+      img.alt = "Finance App";
+    }
   }
 };
 
@@ -16,6 +46,12 @@ export const updateInnerHTML2 = () => {
   const element = document.getElementById("catalog-of-my-things");
   if (element) {
     element.innerHTML = "Ruby Catalog";
+
+    const img = document.querySelector(`img.${element.id}`);
+    if (img) {
+      img.src = ruby;
+      img.alt = "Ruby Catalog";
+    }
   }
 };
 
@@ -23,6 +59,12 @@ export const updateInnerHTML3 = () => {
   const element = document.getElementById("metrics-webapp");
   if (element) {
     element.innerHTML = "Weather App";
+
+    const img = document.querySelector(`img.${element.id}`);
+    if (img) {
+      img.src = weather;
+      img.alt = "Weather App";
+    }
   }
 };
 
@@ -30,5 +72,11 @@ export const updateInnerHTML4 = () => {
   const element = document.getElementById("space-traveller");
   if (element) {
     element.innerHTML = "SpaceTraveller's Hub";
+
+    const img = document.querySelector(`img.${element.id}`);
+    if (img) {
+      img.src = space;
+      img.alt = "SpaceTraveller's Hub";
+    }
   }
 };
